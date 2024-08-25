d89ecb08a3b7bcd9fbe92e9a501a05285149e906
Andre Thomas <drethedevjs@gmail.com>
2024-08-24 23:49:40 -0400

    Added key to Job component interator.

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -6,7 +6,7 @@ const Experience = () => {
     <div className="container mx-auto lg:px-20 px-4">
       <h2 className="mb-5">Experience</h2>
       <div className="indent-2 grid gap-4 lg:ml-12">
-        { jobData.map(jd => <Job job={jd} />) }
+        { jobData.map((jd, idx) => <Job key={idx} job={jd} />) }
       </div>
     </div>
   );
