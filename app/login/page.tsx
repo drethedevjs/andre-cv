cf053a072e09a7ad1d26468c82fbdd022f777b31
Andre Thomas <drethedevjs@gmail.com>
2024-09-05 22:24:23 -0400

    Added login page but currently cannot navigate to it until feature is live.

---
+++ app/login/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { FormEvent, useRef } from "react";
+
+const page = () => {
+  window.location.href = "/";
+  
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
+
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault();
+    alert("logged in!");
+  }
+
+  return (
+    <main className="container mx-auto mt-10">
+      <h1 className="mb-10 text-center">Login</h1>
+      <form className="flex flex-col w-1/2 mx-auto" onSubmit={handleLogin}>
+        <label>Email:</label>
+        <input type="email" ref={emailInputRef} required />
+
+        <label>Password:</label>
+        <input type="password" />
+
+        <input className="btn-invert" type="submit" value="Login" ref={passwordInputRef} required />
+      </form>
+    </main>
+  );
+}
+ 
+export default page;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,25 @@
 function Header() {
   return ( 
     <div className="flex flex-row-reverse p-5 m-5">
+      <div>
+        <a
+          title="Feature coming soon!"
+          className="disable ml-3"
+          // href="/login"
+        >
+          Login
+        </a>
+      </div>
+      <div>
+
       <a
         className="btn" 
         href="https://drive.google.com/file/d/1nwLRBIDH2DdHdd9qMDSnSge1UvkBzoo4/view?usp=sharing"
         target="_blank"
-      >
+        >
         Download Resume
       </a>
+        </div>
     </div>
    );
 }
