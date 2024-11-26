dd3f87cb29b3bf77cc239bef964590505ccf5e2f
Andre Thomas <drethedevjs@gmail.com>
2024-11-25 20:49:16 -0500

    Added the Next Play Project to portfolio section.

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -1,12 +1,36 @@
-import Job from "./Job";
+'use client'
 import jobData from "@/data/jobData";
+import { JobProps } from "@/interfaces/JobProps";
+import { useState } from "react";
+import Job from "./Job";
 
 const Experience = () => {
+  const [ experience, setExperience ] = useState<JobProps[]>(jobData);
+  // setExperience(jobData);
+  // useEffect(() => {
+  //   const getExperience = async () => {
+  //     let res = await fetch("/experience");
+  //     if (!res.ok) {
+  //       console.error("No experience found");
+  //       return;
+  //     }
+
+  //     let data = await res.json();
+  //     setExperience(data);
+  //   }
+
+  //   try {
+  //     getExperience();
+  //   } catch (error: any) {
+  //     console.error(error);
+  //   }
+  // }, []);
+
   return (
     <div className="container mx-auto lg:px-20 px-4">
       <h2 className="mb-5">Experience</h2>
       <div className="indent-2 grid gap-4 lg:ml-12">
-        { jobData.map((jd, idx) => <Job key={idx} job={jd} />) }
+        { experience.map((jd, idx) => <Job key={idx} job={jd} />) }
       </div>
     </div>
   );
--- data/portfolioData.tsx
+++ data/portfolioData.tsx
@@ -1,37 +1,54 @@
 import { WebsiteProps } from "@/interfaces/WebsiteProps";
 
-const portfolioData: Array<WebsiteProps> = [{
-  imageSrc: "/nam-home-page.png",
-  alt: "Screenshot of Not Another Mug Website",
-  classes: "hover:bg-primary rounded-lg",
-  width: 320,
-  height: 221,
-  link: "https://not-another-mug.vercel.app/",
-  name: "Not Another Mug",
-  target: "_blank",
-  technologies: [
-    "React.js",
-    "TypeScript",
-    "Node.js",
-    "Express.js",
-    "MongoDB",
-    "JavaScript"
-  ]
-}, {
-  imageSrc: "/andre-cv-ss.png",
-  alt: "Screenshot of the Andre CV Website",
-  classes: "overlay",
-  width: 320,
-  height: 221,
-  link: "/",
-  name: "Andre CV",
-  target: "_self",
-  technologies: [
-    "Next.js",
-    "TypeScript",
-    "React.js",
-    "JavaScript"
-  ]
-}]
+const portfolioData: Array<WebsiteProps> = [
+  {
+    imageSrc: "/nam-home-page.png",
+    alt: "Screenshot of Not Another Mug Website",
+    classes: "hover:bg-primary rounded-lg",
+    width: 320,
+    height: 221,
+    link: "https://not-another-mug.vercel.app/",
+    name: "Not Another Mug",
+    target: "_blank",
+    technologies: [
+      "React.js",
+      "TypeScript",
+      "Node.js",
+      "Express.js",
+      "MongoDB",
+      "JavaScript"
+    ]
+  },
+  {
+    imageSrc: "/andre-cv-ss.png",
+    alt: "Screenshot of the Andre CV Website",
+    classes: "overlay",
+    width: 320,
+    height: 221,
+    link: "/",
+    name: "Andre CV",
+    target: "_self",
+    technologies: [
+      "Next.js",
+      "TypeScript",
+      "React.js",
+      "JavaScript"
+    ]
+  },
+  {
+    imageSrc: "/nextplayproject-screenshot.png",
+    alt: "Screenshot of the Next Play Project Website",
+    classes: "overlay",
+    width: 320,
+    height: 221,
+    link: "https://nextplayproject.com",
+    name: "The Next Play Project",
+    target: "_blank",
+    technologies: [
+      "Vue.js",
+      "Tailwind"
+    ]
+  }
+]
 
 export default portfolioData;
