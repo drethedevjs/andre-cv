4a616eaf7639bd864b9776a036a815f7f84eb080
Andre Thomas <drethedevjs@gmail.com>
2025-08-15 10:53:00 -0400

    Removed more supabase code.

--- app/auth/logout/route.tsx
+++
@@ -1,8 +0,0 @@
-import { getSupabaseBrowserClient } from "@/app/supabase-utils/browserClient";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-  const supabase = getSupabaseBrowserClient();
-  await supabase.auth.signOut();
-  return NextResponse.redirect(new URL("/", request.url));
-}
