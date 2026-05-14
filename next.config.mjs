a3bb9980669a27d23b1a5f7994de985f58f72cff
Andre Thomas <drethedevjs@gmail.com>
2026-05-14 16:54:51 -0400

    refactor: Remove custom webpack configuration for server externals

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ["@react-pdf/renderer"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals = [...(config.externals || []), { canvas: "canvas" }];
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
