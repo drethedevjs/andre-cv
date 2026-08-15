b09bcac0726071a268b132b77464c189b52a84e2
Andre Thomas <drethedevjs@gmail.com>
2026-08-14 22:21:03 -0400

    chore: Performed dep minor updates.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
+import nextConfig from "eslint-config-next";
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+  ...nextConfig,
   {
     rules: {
       "react/no-unescaped-entities": "off",
